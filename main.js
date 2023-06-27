a28283471c935d58faa41441a689dff154e4efcf
Rully Lukman <rully.lukman@gmail.com>
2023-06-27 20:56:52 +0700

    update code mining difficulty

--- main.js
+++ main.js
@@ -7,10 +7,20 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("Block mined:" + this.hash);
   }
 
 }
@@ -18,6 +28,7 @@ class Block {
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
   }
 
   createGenesisBlock() {
@@ -30,7 +41,8 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    // newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 
@@ -54,14 +66,18 @@ class Blockchain {
 }
 
 let savjeeCoin = new Blockchain();
+
+console.log('Mining block 1....');
 savjeeCoin.addBlock(new Block(1, "10/07/2023", { amount: 4 }))
+
+console.log('Mining block 2....');
 savjeeCoin.addBlock(new Block(2, "10/07/2023", { amount: 10 }))
 
 // console.log(JSON.stringify(savjeeCoin, null, 4))
 
-console.log("Is blockchain valid?", savjeeCoin.isChainValid())
+// console.log("Is blockchain valid?", savjeeCoin.isChainValid())
 
-savjeeCoin.chain[1].data = { amount: 100 }
-savjeeCoin.chain[1].hash = savjeeCoin.chain[1].calculateHash()
+// savjeeCoin.chain[1].data = { amount: 100 }
+// savjeeCoin.chain[1].hash = savjeeCoin.chain[1].calculateHash()
 
-console.log("Is blockchain valid?", savjeeCoin.isChainValid())
+// console.log("Is blockchain valid?", savjeeCoin.isChainValid())
